513c91656c53c8b6511413421d0286fcca0452ec
Sujan Dahal <sujandahal38@gmail.com>
2020-07-02 15:13:43 +0545

    quadratic coefficient :fire

---
+++ src/Coefficients_of_the_quadraticequation.js
@@ -0,0 +1,17 @@
+/*
+In this Kata you are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
+
+Equation will be the form of ax^2 + bx + c = 0
+
+Return type is a Vector containing coefficients of the equations in the order (a, b, c).
+
+Since there are infinitely many solutions to this problem, we fix a = 1.
+
+Remember, the roots can be written like (x-x1) * (x-x2) = 0
+*/
+
+function quadraticEq(x1, x2) {
+    return [1, -(x1+x2), (x1*x2)];
+};
+
+console.log(quadraticEq(1,2););
